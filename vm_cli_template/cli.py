e2affef418e3fdf07f216498f72b66f084288382
MVladislav <dev@mvladislav.online>
2021-07-19 01:03:20 +0200

    refactor: add header

--- vm_cli_template/cli.py
+++ vm_cli_template/cli.py
@@ -11,6 +11,27 @@ from .utilities.utils import Utils
 #
 # ------------------------------------------------------------------------------
 
+# Program Header
+# Basic user interface header
+print(r'''    __  ____    ____          ___      __
+   /  |/  / |  / / /___ _____/ (_)____/ /___ __   __
+  / /|_/ /| | / / / __ `/ __  / / ___/ / __ `/ | / /
+ / /  / / | |/ / / /_/ / /_/ / (__  ) / /_/ /| |/ /
+/_/  /_/  |___/_/\__,_/\__,_/_/____/_/\__,_/ |___/''')
+print('**************** 4D 56 6C 61 64 69 73 6C 61 76 *****************')
+print('****************************************************************')
+print('* Copyright of MVladislav, 2021                                *')
+print('* https://mvladislav.online                                    *')
+print('* https://github.com/MVladislav                                *')
+print('****************************************************************')
+print()
+
+# ------------------------------------------------------------------------------
+#
+#
+#
+# ------------------------------------------------------------------------------
+
 
 class Context:
 
@@ -57,10 +78,12 @@ pass_context = click.make_pass_decorator(Context, ensure=True)
 
 @click.command(cls=ComplexCLI, context_settings=CONTEXT_SETTINGS)
 @click.version_option(VERSION)
-@click.option('-v', '--verbose', help='Enables verbose mode', default=None, count=True)
+@click.option('-v', '--verbose', count=True, help='Enables verbose mode', default=None)
+@click.option('-pom', '--print-only-mode', is_flag=True, help='command wil only printed and not run')
 @pass_context
-def cli(ctx, verbose):
+def cli(ctx, verbose, print_only_mode):
     """Welcome to vm-hack"""
     if verbose != None:
         ctx.verbose = verbose
+    ctx.print_only_mode = print_only_mode
     ctx.utils = Utils(ctx)
