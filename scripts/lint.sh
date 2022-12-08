f9e9123be2f0018049cbbaf6783ab9908021740f
MVladislav <dev@mvladislav.online>
2022-12-08 20:57:54 +0100

    fix(#0): scripts wrong project name

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -6,5 +6,5 @@ python3 -m black vm_cli --check
 python3 -m isort --recursive --check-only vm_cli
 python3 -m flake8 vm_cli --count --select=E9,F63,F7,F82 --show-source --statistics
 python3 -m flake8 vm_cli --count --exit-zero --max-complexity=10 --max-line-length=127 --statistics
-python3 -m pylint --rcfile=setup.cfg "$(find vm_edata_lht_nks -regextype egrep -regex '(.*.py)$')"
+python3 -m pylint --rcfile=setup.cfg "$(find vm_cli -regextype egrep -regex '(.*.py)$')"
 # python3 -m tox --workdir ./scripts/logs/.tox
