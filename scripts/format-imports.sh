f5d1b61181745bbcf5b522ac3185e317544411ff
MVladislav <dev@mvladislav.online>
2022-12-08 20:57:26 +0100

    fix(#0): scripts wrong project name

--- scripts/format-imports.sh
+++ scripts/format-imports.sh
@@ -2,5 +2,5 @@
 set -x
 
 # Sort imports one per line, so autoflake can remove unused imports
-python3 -m isort --recursive --force-single-line-imports --apply vm_edata_lht_nks
+python3 -m isort --recursive --force-single-line-imports --apply vm_cli
 sh ./scripts/format.sh
--- scripts/format.sh
+++ scripts/format.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -e
 set -x
 
-python3 -m autoflake --remove-all-unused-imports --recursive --remove-unused-variables --in-place vm_edata_lht_nks --exclude=__init__.py
-python3 -m black vm_edata_lht_nks
-python3 -m isort --recursive --apply vm_edata_lht_nks
+python3 -m autoflake --remove-all-unused-imports --recursive --remove-unused-variables --in-place vm_cli --exclude=__init__.py
+python3 -m black vm_cli
+python3 -m isort --recursive --apply vm_cli
